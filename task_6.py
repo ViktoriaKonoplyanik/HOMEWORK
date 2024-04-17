886045a4b480cb48eded06f7c607c7a3fe3e8b46
ViktoriaKonoplyanik <v375447086861@gmail.com>
2024-04-17 15:39:15 +0300

    Дан список чисел, отсортированных по возрастанию. На вход принимается значение, равное одному из элементов списка. Реализовать алгоритм бинарного поиска, на выходе программа должна вывести позицию искомого элемента в исходном списке.

---
+++ task_6.py
@@ -0,0 +1,25 @@
+from random import randint
+
+a = []
+for i in range(10):
+    a.append(randint(1,50))
+a.sort()
+print(a)
+
+value = int(input('Введите позицию искомого элемента:  '))
+
+low = 0
+high = len(a) - 1
+mid = (low + high) // 2
+
+while value != a[mid] and low < high:
+    if value > a[mid]:
+        low = mid + 1
+    else:
+         high = mid - 1
+    mid = (low + high) // 2
+
+if value == a[mid]:
+    print('ID:', mid)
+else:
+    print('No value')
