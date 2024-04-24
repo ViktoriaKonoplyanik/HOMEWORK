09db64331950ed2ed45efc4db7dd586ee8caf8e0
ViktoriaKonoplyanik <v375447086861@gmail.com>
2024-04-24 22:36:20 +0300

    1. Дан список чисел. С помощью map() получить список, где каждое число из исходного списка переведено в строку. Пример: на входе – [1, 2, 3], на выходе – [‘1’, ‘2’, ‘3’] 2. Дан список чисел. С помощью filter() получить список тех элементов из исходного списка, значение которых больше 0.

---
+++ unit 7/task_1_and_2.py
@@ -0,0 +1,5 @@
+list_ = [1, 3, 3, 4]
+print(list(map(lambda x: str(x), list_)))
+
+lisr_1 = [2, 3, 5, 7, 9, -22]
+print(list(filter(lambda  x: x > 0, lisr_1)))
---
+++ unit 7/task_3.py
@@ -0,0 +1,10 @@
+def find_palindrome(text):
+
+    text = text.lower()
+    return text == text[::-1]
+
+strings = ['рвалдедлавр', 'лешанаполкеклопанашел', 'abc','аргентинаманитнегра','ишакуказаксенанесказакукаши','python']
+palindromes = list(filter(find_palindrome, strings))
+
+print("Исходный список:", strings)
+print("Палиндромы:", palindromes)
