e604889feeb6b8f271399533cfb2b0d0b753aaa6
ViktoriaKonoplyanik <v375447086861@gmail.com>
2024-05-06 20:57:23 +0300

    Напишите программу с классом Math. При инициализации атрибутов нет. Реализовать методы addition, subtraction, multiplication и division. При передаче в методы двух числовых параметров нужно производить с параметрами соответствующие действия и печатать ответ.

---
+++ unit 11/task_2.py
@@ -0,0 +1,21 @@
+class Math:
+
+    def addition(self, x, y):
+        return x + y
+
+    def subtraction(self, x, y):
+        return x - y
+
+    def multiplication(self, x, y):
+        return x * y
+
+    def division(self, x, y):
+        return x / y
+
+
+
+m = Math()
+print(m.addition(10,5))
+print(m.subtraction(10,5))
+print(m.multiplication(10,5))
+print(m.division(10,5))
